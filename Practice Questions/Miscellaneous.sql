b66d44c97ffe46f1ffbed7c0bb5d1c6b072871d6
shafaq-aslam <shafaqchohan123@gmail.com>
2025-02-06 10:06:23 +0500

    SQL Miscellaneous Prac Ques added

---
+++ Practice Questions/Miscellaneous.sql
@@ -0,0 +1,16 @@
+-- 11. Miscellaneous
+-- Ques 21: Use the LIKE operator to find all employees whose names contain the letter "e."
+-- Ques 22: Find employees whose salaries are within the range 40000 to 80000.
+
+-- Ques 21: Use the LIKE operator to find all employees whose names contain the letter "e."
+USE employees;
+
+SELECT *
+FROM emp_data
+WHERE emp_name LIKE '%s%';
+
+-- Ques 22: Find employees whose salaries are within the range 40000 to 80000.
+
+SELECT * 
+FROM emp_data
+WHERE salary BETWEEN 400.00 AND 800.00;
