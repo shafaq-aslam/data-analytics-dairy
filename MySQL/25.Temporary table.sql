681646d0abafd748e89ffc235782ba52da262d7a
shafaq-aslam <shafaqchohan123@gmail.com>
2025-02-26 10:36:27 +0500

    query added

--- MySQL/25.Temporary table.sql
+++ MySQL/25.Temporary table.sql
@@ -11,4 +11,15 @@ SELECT * FROM temp_table;
 
 INSERT INTO temp_table
 VALUES('Shafaq', 'Red Chilli Pizza', 1500, 'In Process'),
-	  ('Shayan', 'Cheezy Fries', 800, 'Deleverd')
+	  ('Shayan', 'Cheezy Fries', 800, 'Deleverd');
+
+#*************************************
+SELECT * FROM products;
+
+CREATE TEMPORARY TABLE price_over_60
+SELECT *
+FROM products
+WHERE item_price >= 60.00;
+
+SELECT *
+FROM price_over_60;
